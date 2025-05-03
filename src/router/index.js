9fcb7e2feb1ceaed23ea8001421e4ee81c888c0c
Mark Nyon <msnyon@hey.com>
2025-05-03 00:02:09 -0400

    fix: move path prefixing to router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const isStaging = window.location.hostname === 'staging.grandkru.com'
+const base = isStaging ? '/staging' : '/'
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(base),
   routes: [
     {
       path: '/',
