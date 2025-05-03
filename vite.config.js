5118c197631f36cdb89b7108ba9dead68c14586e
Mark Nyon <msnyon@hey.com>
2025-05-03 00:08:16 -0400

    fix: update base URL to /grandkru.com/

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/grandkru.com/',
   server: {
     port: 5173,
     host: true
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
   }
 })
