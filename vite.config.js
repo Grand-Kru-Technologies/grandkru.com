6962318ff93e4a58ada094668d4fccf60eecfb65
Mark Nyon <msnyon@hey.com>
2025-05-03 00:02:09 -0400

    fix: move path prefixing to router configuration

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,13 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/staging/',
   server: {
     port: 5173,
     host: true
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      'vue': 'vue/dist/vue.esm-bundler.js'
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vue': ['vue']
-        }
-      }
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
