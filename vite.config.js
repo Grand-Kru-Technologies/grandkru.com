d6023c097c6584bd9e807380da362875c82dc901
Mark Nyon <msnyon@hey.com>
2025-05-04 14:23:34 -0400

    feat: add debug configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/grandkru.com/',
   server: {
     port: 5173,
     host: true
-  }
+  },
+  build: {
+    sourcemap: true,
+    minify: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  logLevel: 'info'
 })
