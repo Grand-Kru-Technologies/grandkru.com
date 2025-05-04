45a31163b3af5dfb62b87728caa5313978b99a74
Mark Nyon <msnyon@hey.com>
2025-05-04 14:50:24 -0400

    set base to domain name

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { debug } from '../utils/debug'
 
 const isStaging = window.location.hostname === 'staging.grandkru.com'
-const base = isStaging ? '/staging' : '/'
+const base = isStaging ? '/grandkru.com/' : '/'
+
+debug.log('Router initialized with base:', base)
 
 const router = createRouter({
   history: createWebHistory(base),
@@ -22,6 +25,24 @@ const router = createRouter({
       component: () => import('../views/ContactView.vue')
     }
   ]
+})
+
+// Add navigation logging
+router.beforeEach((to, from, next) => {
+  debug.log('Navigation:', {
+    from: from.path,
+    to: to.path,
+    params: to.params,
+    query: to.query
+  })
+  next()
+})
+
+router.afterEach((to, from) => {
+  debug.log('Navigation completed:', {
+    from: from.path,
+    to: to.path
+  })
 })
 
 export default router
