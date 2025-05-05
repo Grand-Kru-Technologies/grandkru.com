9c8e581bad27551d7ab4ec39e6ae5b4949910112
Mark Nyon <msnyon@hey.com>
2025-05-04 22:33:23 -0400

    set logLevel to debug

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/',
-  server: {
-    port: 5173,
-    host: true
-  },
-  build: {
-    sourcemap: true,
-    minify: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    base: '/',
+    server: {
+      port: 5173,
+      host: true
+    },
+    build: {
+      sourcemap: true,
+      minify: false,
+      rollupOptions: {
+        output: {
+          manualChunks: undefined
+        }
       }
+    },
+    logLevel: 'debug',
+    // Expose env variables to the client
+    define: {
+      'process.env': env
     }
-  },
-  logLevel: 'info'
+  }
 })
