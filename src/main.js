bb619795bdc6ef4914999eef410f2a4a2f32a5df
Mark Nyon <msnyon@hey.com>
2025-05-04 23:01:05 -0400

    WIP

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+import { debug } from './utils/debug'
 
 const app = createApp(App)
 
+debug.log('Application initialized')
+
 app.use(router)
 
+debug.log('Router mounted')
+
 app.mount('#app')
+
+debug.log('Application mounted to DOM')
