03526916722f974bfb7eb00e53e1e4710c4442fc
Mark Nyon <msnyon@hey.com>
2025-06-06 20:05:19 -0400

    fix: added Contact Us header to the view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen from-primary/5 to-primary/10 py-16">
     <div class="container mx-auto px-4">
-
+      <h1 class="text-4xl font-bold text-primary text-center mb-12">Contact Us</h1>
 
       <!-- Success Message -->
       <div v-if="showSuccess"
